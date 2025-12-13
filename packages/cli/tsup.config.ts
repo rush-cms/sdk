de71b3d6cb43d7ba8348e41872c43828a01754cd
Rafhael Marsigli <rmarsigli@gmail.com>
2025-12-13 01:53:25 -0300

    fix(cli): configure as executable-only package without library exports

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['cjs', 'esm'],
-    dts: true,
+    format: ['cjs'],
+    dts: false,
     splitting: false,
     sourcemap: true,
     clean: true,
